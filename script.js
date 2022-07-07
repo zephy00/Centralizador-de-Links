ca6a8fc976e22f6096c3fa13ac38b2bcb3c66888
Ayan Marques <92836527+zephy00@users.noreply.github.com>
2022-07-07 10:58:56 -0300

    dark e light mode

---
+++ script.js
@@ -0,0 +1,16 @@
+const botaoDark = document.getElementById('toggle-dark');
+
+// Verifica se tem no localStorage seleção do dark theme
+if (localStorage.getItem('data-theme') == 'dark') {
+  botaoDark.checked = true;
+}
+
+// Liga a função ao checkbox
+botaoDark.addEventListener('change', () => {
+    let theme = localStorage.getItem('data-theme'); // Retrieve saved them from local storage
+    if (!botaoDark.checked){
+        changeThemeToLight()
+    }else{
+        changeThemeToDark()
+    }
+});
